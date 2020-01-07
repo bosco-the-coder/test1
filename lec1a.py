b511d0ca84d865d1ccbb3eed0d7bb70dbf243889
yanko leskovar <Yanko@yankos-iMac.local>
2020-01-08 02:47:04 +1100

    changed 1a file with comments

--- lec1a.py
+++ lec1a.py
@@ -3,6 +3,7 @@ radius = 2.2
 # area of circle equation <- this is a comment
 area = pi*(radius**2)
 print(area)
+# addiing some more comments from user-b
 
 # change values of radius <- another comment
 # use comments to help others understand what you are doing in code
